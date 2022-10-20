9b2ab81e0ecf10549c8ce06c931046ba856b2658
Sam Hertzler <hertilizer@gmail.com>
2022-10-20 12:18:14 -0500

    pull card content from Fauna

--- src/SelectQuiz.js
+++ src/SelectQuiz.js
@@ -1,26 +1,31 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import { Grid, Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Grid, Box } from '@mui/material';
 import NameCard from './Name_Card';
 
 const faunadb = require('faunadb');
 const client = new faunadb.Client({
-    secret: process.env.REACT_APP_DB_KEY,
-    endpoint: "https://db.fauna.com/",
-})
+  secret: process.env.REACT_APP_DB_KEY,
+  endpoint: "https://db.fauna.com/",
+});
 
-const {
-    // Ref,
-    Paginate,
-    // Get,
-    Match,
-    Index,
-    // Create,
-    // Collection,
-    // Join,
-    // Call,
-    // Function: Fn,
-} = faunadb.query;
+var q = faunadb.query;
+var data;
+client.query(
+  q.Paginate(
+    q.Match(
+      q.Index("all_quizzes")
+    )
+  )
+)
+.then(
+  function (response) {
+    data = response.data;
+  },
+  function () {
+    console.log("Query failed!");
+  }
+);
 
 // xs, extra-small: 0px
 // sm, small: 600px
@@ -29,78 +34,48 @@ const {
 // xl, extra-large: 1536px
 
 const GridStyles = {
-    width:"100%",
-    backgroundColor: "lightblue",
-    paddingRight: {
-        xs: 10,
-        sm: 10,
-        md: 10,
-        lg: 10,
-        xl: 10,
-    },
-    paddingBottom: 10,
-    marginTop: 2,
-    marginLeft: "auto",
-    marginRight: "auto",
+  width: "100%",
+  backgroundColor: "lightblue",
+  paddingRight: {
+    xs: 10,
+    sm: 10,
+    md: 10,
+    lg: 10,
+    xl: 10,
+  },
+  paddingBottom: 10,
+  marginTop: 2,
+  marginLeft: "auto",
+  marginRight: "auto",
 }
 
 const SelectQuiz = () => {
-    var quizzes;
-    // var quizQuery = 
-    // client.query(
-    //     Paginate(
-    //         Match(
-    //             Index("all_quizzes")
-    //         )
-    //     )
-    // )
-    // .then(function (response) {
-    //     quizzes = response.data;
-    //     console.log(response.ref);
-    // });
+  const [quizzes, setQuizzes] = useState(data);
 
-    quizzes = [
-        ["Alabama Water Demo", "Alabama Quiz for Demo", 1, "rivers"],
-        ["Alabama Water Demo", "Alabama Quiz for Demo", 1, "states"],
-        ["Alabama Water Demo", "Alabama Quiz for Demo (update)", 1, "rivers"],
-        ["Alabama Water Demo", "Alabama Quiz for Demo (update)", 1, "states"],
-        ["Great Lakes Quiz", "Great Lakes Quiz", 1, "lakes"],
-        ["Sample Quiz 2", "QUIZ_DESCRIPTION", 0, "INSERT_TAG_NAME"],
-        ["TITLE_TO_DISPLAY", "QUIZ_DESCRIPTION", 0, "INSERT_TAG_NAME"]
-    ];
-
-    return(
-        <Box sx={{
-        width: '100%',
-        overflow: "auto", 
-        }}>
-            <Grid container 
-            columns={{ xs: 2, md: 3, xl: 4 }}
-            spacing={10}
-            sx={GridStyles}>
-                {quizzes.map((quiz, index) => (
-                    <Grid item
-                    key = {index}
-                    xs = {2}
-                    sm = {1}>
-                        <NameCard 
-                        title={quiz[0]}
-                        description={quiz[1]}
-                        difficulty={quiz[2]}
-                        tags={quiz[3]} />
-                    </Grid>
-                ))}
-                {/* {Array.from(Array(6)).map((quiz, index) => (
-                    <Grid item
-                    key = {index}
-                    xs = {2}
-                    sm = {1}>
-                        <NameCard name={"Number " + index} />
-                    </Grid>
-                ))} */}
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box sx={{
+      width: '100%',
+      overflow: "auto",
+    }}>
+      <Grid container
+        columns={{ xs: 2, md: 3, xl: 4 }}
+        spacing={10}
+        sx={GridStyles}>
+        {Array.from(quizzes).map((quiz, index) => (
+          <Grid item
+            key={index}
+            xs={2}
+            sm={1}>
+            <NameCard
+              title={quiz[0]}
+              description={quiz[1]}
+              difficulty={quiz[2]}
+              tags={quiz[3]} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
 
 export default SelectQuiz;
